9f96ed3f63d58538e1dbc678464422653789682d
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-03 17:34:37 +0200

    feat: add Kafka Streams function to filter PageEvent by duration and count occurrences

--- src/main/java/com/proj/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/com/proj/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -1,12 +1,18 @@
 package com.proj.kafkaspringcloudstream.handlers;
 
 import com.proj.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 @Component
 public class PageEventHandler {
@@ -23,6 +29,16 @@ public class PageEventHandler {
     @Bean
     public Supplier<PageEvent> pageEventSupplier(){
         return () -> new PageEvent(Math.random()>0.5?"P11":"P2",Math.random()>0.5?"U1":"U2",new Date(),1000);
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> KStreamFunction(){
+        return (input) ->
+                input.filter((k,v)->v.duration()>100)
+                .map((k,v)->new KeyValue<>(v.name(),v.duration()))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .count()
+                        .toStream();
     }
 
 }
